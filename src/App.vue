8d6575dc9d7c2303ec85664f983091b5446ab2a5
Gianluca Benassi <benox1199@gmail.com>
2021-12-16 13:43:07 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Header from './components/macro/Header.vue';
+import Main from './components/macro/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
